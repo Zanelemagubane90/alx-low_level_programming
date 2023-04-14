53049581d4191af57f5dce00d85a62c67467d1db
Zanelemagubane90 <zmagubane90@gmail.com>
2023-04-13 18:48:59 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,11 +11,11 @@
 
 int main(int argc, char *argv[])
 {
-	int one = 1024, int_checker = -1, two = 0, int_from, int_t;
+	int one = 1024, int_checker = -1, two = 0, int_from, int_to;
 	char buffer[1024];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp int_from int_t\n"), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp int_from int_to\n"), exit(97);
 	int_from = open(argv[1], O_RDONLY);
 	if (int_from == int_checker)
 	{
@@ -23,8 +23,8 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	int_t = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (int_t == int_checker)
+	int_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (int_to == int_checker)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(int_from), exit(99);
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
 			exit(98);
 		}
 
-		two = write(int_t, buffer, one);
+		two = write(int_to, buffer, one);
 		if (two < one)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
@@ -47,8 +47,8 @@ int main(int argc, char *argv[])
 	if (close(int_from) == int_checker)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", int_from), exit(100);
 
-	if (close(int_t) == int_checker)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", int_t), exit(100);
+	if (close(int_to) == int_checker)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", int_to), exit(100);
 
 	return (0);
 }
